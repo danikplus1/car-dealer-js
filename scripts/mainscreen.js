2d68782257e1c6acbd5928985994ef18cfd44f24
danikplus1 <danik.solovei@gmail.com>
2024-04-26 20:33:19 +0200

    add acc block and behavior for buttons

--- scripts/mainscreen.js
+++ scripts/mainscreen.js
@@ -153,6 +153,48 @@ document.addEventListener("DOMContentLoaded", function () {
             $day
           );
           $main.append($clientForm);
+
+          //add acc block
+
+          const $acc = document.createElement("div");
+          const $tires = document.createElement("div");
+          const $tiresInfo = document.createElement("p");
+          $tiresInfo.textContent = "Winter tires 2000 PLN";
+          const $tiresButton = document.createElement("button");
+          $tiresButton.textContent = "+";
+          $tiresButton.addEventListener("click", function () {
+            $tiresButton.classList.toggle("clicked");
+            if ($tiresButton.classList.contains("clicked")) {
+              $tiresButton.textContent = "-";
+            } else {
+              $tiresButton.textContent = "+";
+            }
+          });
+
+          const $warranty = document.createElement("div");
+          const $warrantyInfo = document.createElement("p");
+          $warrantyInfo.textContent = "One year warranty 5000 PLN";
+          const $warrantyButton = document.createElement("button");
+          $warrantyButton.textContent = "+";
+          $warrantyButton.addEventListener("click", function () {
+            $warrantyButton.classList.toggle("clicked");
+            if ($warrantyButton.classList.contains("clicked")) {
+              $warrantyButton.textContent = "-";
+            } else {
+              $warrantyButton.textContent = "+";
+            }
+          });
+
+          $tires.append($tiresInfo, $tiresButton);
+          $warranty.append($warrantyInfo, $warrantyButton);
+          $acc.append($tires, $warranty);
+          $main.append($acc);
+
+          // add purchase block
+          const $purchase = document.createElement("button");
+          $purchase.textContent = "Purchase";
+
+          $main.append($purchase);
         }
 
         $article.addEventListener("click", clickHandler);
