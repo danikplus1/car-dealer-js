d00f531e9d4da68365b6787dbbbd2235c17d547a
danikplus1 <danik.solovei@gmail.com>
2024-06-21 23:22:41 +0200

    add tailwind CSS

--- scripts/changeTitle.js
+++
@@ -1,9 +0,0 @@
-export default function changeTitle($article) {
-  const $carBrand = $article.querySelector(".car-brand");
-  let carBrandText = $carBrand.textContent;
-  carBrandText = carBrandText.replace("Brand: ", "");
-  const $carModel = $article.querySelector(".car-model");
-  let carModelText = $carModel.textContent;
-  carModelText = carModelText.replace("Model: ", "");
-  document.title = `${carBrandText} ${carModelText} - Car Details`;
-}
